e03da99ca4fff3630be8784337d425caab2c504d
Daniel Verkamp <daniel.verkamp@intel.com>
2018-04-20 13:29:43 -0700

    doc: update to ddeaeeec193b66d6f97973a6402f88b5d481d14c
    Change-Id: I8ed2a8aac8a57d4ad4de6177d60c53b85988a525
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- doc/search/pages_a.js
+++ doc/search/pages_a.js
@@ -3,5 +3,6 @@ var searchData=
   ['spdk_20directory_20structure',['SPDK Directory Structure',['../directory_structure.html',1,'']]],
   ['storage_20performance_20development_20kit',['Storage Performance Development Kit',['../index.html',1,'']]],
   ['spdk_20porting_20guide',['SPDK Porting Guide',['../porting.html',1,'']]],
+  ['spdk_20cli',['SPDK CLI',['../spdkcli.html',1,'']]],
   ['ssd_20internals',['SSD Internals',['../ssd_internals.html',1,'']]]
 ];
