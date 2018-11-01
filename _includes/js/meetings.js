da9a62878dc8b6c6353fb76f3dd5dd0a05c7f6f6
Jim Harris <james.r.harris@intel.com>
2018-10-31 21:38:03 -0700

    Use getUTC variants when recalculating to account for DST.
    Fixes bug observed where Asia meetings were showing up
    as Tuesdays instead of Wednesdays for US team members.
    
    Signed-off-by: Jim Harris <james.r.harris@intel.com>
    Change-Id: I5aaaf298717ae944d632ca6f2758826d8016d979

--- _includes/js/meetings.js
+++ _includes/js/meetings.js
@@ -24,9 +24,9 @@ while (asiaBugDate < currentDate) {
 
 // Regenerate the dates here - this makes sure we account for daylight savings adjustments between the starting
 // date at the beginning of this file and the calculated address based on the current date/time.
-euroDate = new Date(Date.UTC(euroDate.getFullYear(), euroDate.getMonth(), euroDate.getDate(), 15, 0, 0, 0));
-asiaDate = new Date(Date.UTC(asiaDate.getFullYear(), asiaDate.getMonth(), asiaDate.getDate(), 4, 0, 0, 0));
-asiaBugDate = new Date(Date.UTC(asiaBugDate.getFullYear(), asiaBugDate.getMonth(), asiaBugDate.getDate(), 4, 30, 0, 0));
+euroDate = new Date(Date.UTC(euroDate.getUTCFullYear(), euroDate.getUTCMonth(), euroDate.getUTCDate(), 15, 0, 0, 0));
+asiaDate = new Date(Date.UTC(asiaDate.getUTCFullYear(), asiaDate.getUTCMonth(), asiaDate.getUTCDate(), 4, 0, 0, 0));
+asiaBugDate = new Date(Date.UTC(asiaBugDate.getUTCFullYear(), asiaBugDate.getUTCMonth(), asiaBugDate.getUTCDate(), 4, 30, 0, 0));
 
 document.getElementById("euro-mtg").textContent = formatDate(euroDate);
 document.getElementById("asia-mtg").textContent = formatDate(asiaDate);
