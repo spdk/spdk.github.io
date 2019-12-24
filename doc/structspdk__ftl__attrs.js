e3103189747b5ee3f58f97c143f5cb595a5df9d2
SPDK Nightly Cron Job <sys_sgsw@intel.com>
2019-12-24 05:00:39 -0700

    doc: update to 005d87ed4dea0f591b5af432b8dd57219fcd9dc9
    Change-Id: I79da2336289f6d79d9deee77886e890f4cce5340
    Signed-off-by: SPDK Nightly Cron Job <sys_sgsw@intel.com>

--- doc/structspdk__ftl__attrs.js
+++ doc/structspdk__ftl__attrs.js
@@ -1,11 +1,11 @@
 var structspdk__ftl__attrs =
 [
     [ "cache_bdev_desc", "structspdk__ftl__attrs.html#a591b3cc04e004e8d7ddb534947708d8d", null ],
-    [ "chunk_size", "structspdk__ftl__attrs.html#aa76d7d809b607681125285504d246c4b", null ],
     [ "conf", "structspdk__ftl__attrs.html#a4d113de40e95d6b1b4e3bb97f2a6231e", null ],
     [ "lbk_cnt", "structspdk__ftl__attrs.html#a00c7ee461678750becaefbc9608d58c2", null ],
     [ "lbk_size", "structspdk__ftl__attrs.html#ab5607984fcfd9302e2b1333d7951abd2", null ],
-    [ "num_chunks", "structspdk__ftl__attrs.html#a1e4144eada0f6b0302a73d2fb6eefa79", null ],
+    [ "num_zones", "structspdk__ftl__attrs.html#ae1bfc8ee509fbbdcf33264b92f4060b9", null ],
     [ "range", "structspdk__ftl__attrs.html#afebcc710fe6130d60380b9b55a26f057", null ],
-    [ "uuid", "structspdk__ftl__attrs.html#ab398d419db70dd7e3de3d69c676a2723", null ]
+    [ "uuid", "structspdk__ftl__attrs.html#ab398d419db70dd7e3de3d69c676a2723", null ],
+    [ "zone_size", "structspdk__ftl__attrs.html#a171ec0781950b25e61bbf192b8cda73c", null ]
 ];
