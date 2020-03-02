3de32e47c434ad9c7a9b0673baf3462c38f097b2
SPDK Nightly Cron Job <sys_sgsw@intel.com>
2020-03-02 05:00:32 -0700

    doc: update to 9cc1013a8340e08da424ebf5a0071d05f9e8816a
    Change-Id: I1528b4d36927dcc41b419f0e1587bd673346dea7
    Signed-off-by: SPDK Nightly Cron Job <sys_sgsw@intel.com>

--- doc/structspdk__ftl__conf.js
+++ doc/structspdk__ftl__conf.js
@@ -6,12 +6,12 @@ var structspdk__ftl__conf =
     [ "lba_rsvd", "structspdk__ftl__conf.html#a529cbd0089f91d8d6249263f8e40ead4", null ],
     [ "limits", "structspdk__ftl__conf.html#a0ea94001a8f25e32ea91bc919b8830c1", null ],
     [ "max_active_relocs", "structspdk__ftl__conf.html#a328e94a59f30ee22d9e562eb0816f836", null ],
+    [ "max_io_channels", "structspdk__ftl__conf.html#a383d7e6efd84a1ff5c94ae0502531d19", null ],
     [ "max_reloc_qdepth", "structspdk__ftl__conf.html#af6ce7933ea875575b107dc20384a41a7", null ],
     [ "max_request_cnt", "structspdk__ftl__conf.html#ab616d5c9c1d384b4bd1c42cabdbc47e2", null ],
     [ "max_request_size", "structspdk__ftl__conf.html#a1f728a4303cf103117d3b1dfbd93d62a", null ],
-    [ "num_interleave_units", "structspdk__ftl__conf.html#accce5642e65fa0802f480315ce2d7999", null ],
     [ "nv_cache", "structspdk__ftl__conf.html#aa77e08d7956329898b0c64aa1b137adf", null ],
-    [ "rwb_size", "structspdk__ftl__conf.html#af873e1c7d113de72fdf351479442744a", null ],
     [ "use_append", "structspdk__ftl__conf.html#a616721ee96e7cd1f7721ed19f59dc247", null ],
-    [ "user_io_pool_size", "structspdk__ftl__conf.html#a1806a31e7d6c83f86d598e477474c102", null ]
+    [ "user_io_pool_size", "structspdk__ftl__conf.html#a1806a31e7d6c83f86d598e477474c102", null ],
+    [ "write_buffer_size", "structspdk__ftl__conf.html#ab9c76758916fcabfe00ac708f4b3b674", null ]
 ];
