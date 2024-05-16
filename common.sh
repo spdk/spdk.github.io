b26449a1073b8cdc2b7f4f065583f818ad038a27
Krzysztof Karas <krzysztof.karas@intel.com>
2024-05-16 14:12:21 +0000

    doc: align header with rest of the pages
    Unlike other pages, documentation in SPDK uses hardcoded menu entries
    (see _doc_header.html). This means that it is required to maintain
    two menus separately. To amend this problem use a small Jekyll build
    to generate a new header. That way we can reuse entries from _config.yml.
    
    Also bump versions of jquery, popper and bootstrap.
    
    Change-Id: I9acde76a0d8a37239cc50b4289933504cc7bbc0d
    Signed-off-by: Krzysztof Karas <krzysztof.karas@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk.github.io/+/19501
    Reviewed-by: Ben Walker <ben@nvidia.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Jim Harris <jim.harris@samsung.com>

--- common.sh
+++ common.sh
@@ -11,8 +11,31 @@ remove_repo() {
 }
 
 regenerate_docs() {
+	doc_header_dir="doc_header"
+	rm -rf $doc_header_dir
+
+	mkdir -p $doc_header_dir/_layouts
+
+	# _includes/header.html contains jekyll directives for marking menu entries as active.
+	# If we want to have headers to show "DOCUMENTATION" entry as active, we need to have
+	# a 'doc.md' file, which will result in jekyll generating 'doc' directory and match
+	# with box URL.
+	echo "---
+layout: doc
+title: Storage Performance Development Kit
+---" > $doc_header_dir/doc.md
+
+	cp _layouts/doc.html $doc_header_dir/_layouts
+	cp -R _includes $doc_header_dir/_includes
+	cp _config.yml $doc_header_dir
+	# Use Jekyll to generate a header for docs using _config.yml
+	(cd $doc_header_dir; jekyll build)
+
+	# Clean empty and blank lines
+	sed -ri '/^\s*$/d' $doc_header_dir/_site/doc/index.html
+
 	# Overwrite header and footer with the spdk.io versions
-	cp _doc_header.html $repo/doc/header.html
+	cp $doc_header_dir/_site/doc/index.html $repo/doc/header.html
 	cp _doc_footer.html $repo/doc/footer.html
 	cp css/spdk.css $repo/doc/spdk.css
 	sed -i 's/^HTML_EXTRA_STYLESHEET.*/HTML_EXTRA_STYLESHEET  = spdk.css/' $repo/doc/Doxyfile
@@ -22,5 +45,6 @@ regenerate_docs() {
 	cp css/navtree.css $repo/doc/output/html
 
 	rm -rf doc/*
+	rm -r $doc_header_dir
 	cp -R $repo/doc/output/html/* doc
 }
