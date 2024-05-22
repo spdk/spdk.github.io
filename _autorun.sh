ad151d5193e47304a9b22cdcdfd2929c33716e2b
Krzysztof Karas <krzysztof.karas@intel.com>
2024-05-22 07:53:11 +0000

    _autorun.sh: add regenerating docs
    This will let us see results of changes in documentation header/footer
    without a need to update the repository.
    
    Change-Id: Ib7178306705c7a536f4d7ec4200f8491fbf90aea
    Signed-off-by: Krzysztof Karas <krzysztof.karas@intel.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk.github.io/+/23349
    Reviewed-by: Jim Harris <jim.harris@samsung.com>
    Reviewed-by: Kamil Godzwon <kamilx.godzwon@intel.com>
    Reviewed-by: Karol Latecki <karol.latecki@intel.com>
    Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>
    Reviewed-by: Ben Walker <ben@nvidia.com>

--- _autorun.sh
+++ _autorun.sh
@@ -4,5 +4,10 @@ set -ex
 
 src=$(readlink -f $(dirname $0))
 dst=.
+source $src/common.sh
+
+check_clone_repo
+regenerate_docs
+remove_repo
 
 jekyll build --source "$src" --destination "$dst"/_site
